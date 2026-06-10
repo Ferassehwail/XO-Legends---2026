86b55f8e00e156911d5f2e6b3c7970a7394969a5
Ferassehwail <98327438+Ferassehwail@users.noreply.github.com>
2026-06-10 10:13:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'xo-cyber-v2';
+const assets = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
